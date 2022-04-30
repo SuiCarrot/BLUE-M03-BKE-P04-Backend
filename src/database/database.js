23c8fc75711715b5ce4dd3b5b76a631a20b00517
SuiCarrot <91708805+SuiCarrot@users.noreply.github.com>
2022-04-29 21:37:46 -0300

    feat(database): Conexão mongo atlas e .env

--- src/database/database.js
+++ src/database/database.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-function connectToDatabase() {
-    mongoose
-      .connect('mongodb+srv://Bruno123:blue123@api-rickandmorty.pbiml.mongodb.net/api-rickAndMorty?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => {
-        console.log('Mongo Atlas conectado com sucesso');
-      })
-      .catch((err) => {
-        return console.log(`Erro na conexao com o banco: ${err}`);
-      });
-  }
-  
-module.exports = connectToDatabase;
+const connectDatabase = () => {
+  console.log("Conectando ao banco de dados...");
+
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Mongo Atlas conectado com sucesso!"))
+    .catch((err) => console.log(`Erro ao conectar com o banco: ${err}`));
+
+};
+
+module.exports = connectDatabase;
