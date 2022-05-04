015d1d99434b57910302863d1c35ff70004e54e7
ddanilooalves <daniloalves0512@gmail.com>
2022-05-04 18:51:54 -0300

    add swagger

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth-controller");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.post("/auth/login", authController.loginController);
 
--- src/routes/users-route.js
+++ src/routes/users-route.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users-controller');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.get('/users', usersController.findAllUsersController);
 
